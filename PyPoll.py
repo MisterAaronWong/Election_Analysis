16b1c3a90399f6ec2bb3896a911c5dfd37b9efae
MisterAaronWong <mr.wongaaron@gmail.com>
2021-10-18 20:07:13 -0400

    update at 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -32,15 +32,23 @@ import csv
 import os
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     #Print the file object
-    print(election_data)
+    ## print(election_data)
+    # To do: read and analyze the data here. 3.4.4
+    # Read the file object with the reader function. 3.4.4
+    file_reader = csv.reader(election_data)
+    # Read and Print the header row
+    headers = next(file_reader)
+    print(headers)
 
 # Create a filename variable to a direct or indirect path to the file
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+## file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Using the open() function with the "w" mode we will write data to the file
-open(file_to_save, "w")
+## open(file_to_save, "w")
 
 # Create a filename variable to a direct or indirect path to the file.
 ## file_to_save = os.path.join("analysis", "election_analysis.txt")
@@ -53,9 +61,9 @@ open(file_to_save, "w")
 ## outfile.close()
 
 # Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+## file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Using the with statement open the file as a text file
-with open(file_to_save, "w") as txt_file:
+## with open(file_to_save, "w") as txt_file:
     # Write some data to the file.
     ## txt_file.write("Hello World")
     # Write three counties to the file
@@ -63,6 +71,6 @@ with open(file_to_save, "w") as txt_file:
     ## txt_file.write("Denver, ")
     ## txt_file.write("Jefferson")
     # Write three counties to the file.
-    txt_file.write("Counties in the Election")
-    txt_file.write("\n-----------------")
-    txt_file.write("\nArapahoe\nDenver\nJefferson")
+    ## txt_file.write("Counties in the Election")
+    ## txt_file.write("\n-----------------")
+    ## txt_file.write("\nArapahoe\nDenver\nJefferson")
